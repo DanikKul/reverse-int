333ebda9e9c4190c1cd68f7a3ed70c449c1a080c
DanikKul <92942037+DanikKul@users.noreply.github.com>
2022-01-08 11:27:57 +0300

    feat: add a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+       let res = 0, k = 1;
+       n = Math.abs(n);
+       while(n >= 1){
+           res += n % 10;
+           n = (n - (n % 10)) / 10;
+           res *= 10;
+           k++;
+       }
+       return res / 10;
 }
